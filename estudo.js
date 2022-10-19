3b0ac2b809fe46cdc78eb02b39c72bbf463c4390
sophiasoncin <110677705+sophiasoncin1234@users.noreply.github.com>
2022-10-19 08:08:26 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -42,3 +42,20 @@ function calcula(){
    document.write ("Resultado:" + r); 
 }
 
+let op = " ";
+function operacao (ope){
+   op = ope;
+}
+function calcule (){
+   let v1 = document.getElementById ("v1").value
+   let v2 = document.getElementById ("v2").value
+   if(op == "+"){
+      r = Number(v1) + Number(v2);
+   }
+
+   document.getElementById ("resultado").innerHTML = r;
+
+
+
+
+}
